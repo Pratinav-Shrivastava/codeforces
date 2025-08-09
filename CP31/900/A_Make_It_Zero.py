e06a45790eaa22f4fd87b0574d93f708be13f887
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2025-08-09 16:07:34 +0530

    1869A - Make It Zero

---
+++ CP31/900/A_Make_It_Zero.py
@@ -0,0 +1,17 @@
+def main(n):
+    if n%2 == 0:
+        print(2)
+        print(1, n)
+        print(1, n)
+    else:
+        print(4)
+        print(1, n - 1)
+        print(1, n - 1)
+        print(n - 1, n)
+        print(n - 1, n)
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    _ = list(map(int, input().split()))
+    main(n)
