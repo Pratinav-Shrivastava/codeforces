d9d8fe70e8b22b91bd576602cef00e03ffc46a67
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2025-08-27 13:18:46 +0530

    2134A - Painting With Two Colors another way

--- Contests/2134/A_Painting_With_Two_Colors.py
+++ Contests/2134/A_Painting_With_Two_Colors.py
@@ -1,7 +1,13 @@
 def painting(n, a, b):
-    blue_in_center = ((n - b) & 1) == 0
-    red = (a <= b) or (((a - b) & 1) == 0)
-    return("YES" if blue_in_center and red else "NO")
+    if a <= b:
+        valid = (n-b)%2 == 0
+    else:
+        valid = (n-a)%2 == 0 and (n-b)%2 == 0
+    return("YES" if valid else "NO")
+    
+    # blue_in_center = ((n - b) & 1) == 0
+    # red = (a <= b) or (((a - b) & 1) == 0)
+    # return("YES" if blue_in_center and red else "NO")
 
 t = int(input())
 for _ in range(t):
