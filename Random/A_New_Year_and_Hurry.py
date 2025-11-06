65a90f65ef1813984879580e01e580260d5db354
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2025-11-06 23:04:22 +0530

    750A - New Year and Hurry

--- Random/A_New_Year_and_Hurry.py
+++ Random/A_New_Year_and_Hurry.py
@@ -1,8 +1,19 @@
+import math
+
 n, k = map(int, input().split())
-time_remaining = 60*4
-time_remaining -= k
-i = 1
-while time_remaining - 5*i >= 0 and i <= n:
-    time_remaining -= 5*i
-    i += 1
-print(i-1)
+T = 240 - k
+if T <= 0:
+    print(0)
+else:
+    # i = floor((-5 + sqrt(25 + 40*T)) / 10)
+    i = ( -5 + math.sqrt(25 + 40*T) ) // 10  # integer-safe
+    print(int(min(n, i)))
+
+# n, k = map(int, input().split())
+# time_remaining = 60*4
+# time_remaining -= k
+# i = 1
+# while time_remaining - 5*i >= 0 and i <= n:
+#     time_remaining -= 5*i
+#     i += 1
+# print(i-1)
