39c14a00b1f48bf52dee97e3528bdcee44b6b0b3
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-04-15 03:52:00 +0530

    1999C - Showering

---
+++ Random/C_Showering.py
@@ -0,0 +1,12 @@
+def solve():
+    n, s, m = map(int, input().split())
+    segs = [[0, 0], [m, m]] + [list(map(int, input().split())) for i in range(n)]
+    segs.sort()
+    for i in range(1, n + 2):
+        if segs[i][0] - segs[i - 1][1] >= s:
+            print('YES')
+            return
+    print('NO')
+
+for _ in range(int(input())):
+    solve()
