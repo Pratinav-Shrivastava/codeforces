85baaca6a82ee576d4ad6ada82d236f3faf819e2
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-04-29 15:11:57 +0530

    2072A - New World, New Me, New Array

---
+++ Random/A_New_World_New_Me_New_Array.py
@@ -0,0 +1,10 @@
+def solve():
+    n, k, p = map(int, input().split())
+    if -n * p <= k <= n * p:
+        print((abs(k) + p - 1) // p)
+    else:
+        print(-1)
+
+t = int(input())
+for _ in range(t):
+    solve()
