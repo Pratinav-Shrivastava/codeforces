d6ce1d009f274baacc8c64a0cc9c2b756d268454
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-05-17 03:20:20 +0530

    1980B - Choosing Cubes

---
+++ Random/B_Choosing_Cubes.py
@@ -0,0 +1,17 @@
+def solve():
+    n, f, k = map(int, input().split())
+    f -= 1
+    k -= 1
+    a = list(map(int, input().split()))
+    x = a[f]
+    a.sort(reverse=True)
+    if a[k] > x:
+        print("NO")
+    elif a[k] < x:
+        print("YES")
+    else:
+        print("YES" if k == n - 1 or a[k + 1] < x else "MAYBE")
+ 
+t = int(input())
+for _ in range(t):
+    solve()
