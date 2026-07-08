4c574186a5423f6b115560e35ab578b9ca7aa9f5
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-07-08 20:11:15 +0530

    1932A - Thorns and Coins

---
+++ Random/A_Thorns_and_Coins.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    s = input().strip()
+
+    ans = 0
+    for i in range(1, n):
+        if s[i] == '@':
+            ans += 1
+        if s[i] == '*' and s[i - 1] == '*':
+            break
+
+    print(ans)
