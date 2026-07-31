6b65e86ccdcb09e6a7355519b5065567113b03df
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-07-31 09:18:12 +0530

    2001A - Make All Equal

---
+++ Random/A_Make_All_Equal.py
@@ -0,0 +1,11 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+
+    freq = [0] * (n + 1)
+    for x in a:
+        freq[x] += 1
+
+    print(n - max(freq))
