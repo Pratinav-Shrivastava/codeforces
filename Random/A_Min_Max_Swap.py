9f0b1978400af72e58fabaf3ec1add0349d6b5c6
Pratinav-Shrivastava <pratinavshrivastava6@gmail.com>
2026-09-12 02:42:39 +0530

    1631A - Min Max Swap

---
+++ Random/A_Min_Max_Swap.py
@@ -0,0 +1,19 @@
+def calc_max(a):
+    return max(a)
+
+
+tc = int(input())
+
+while tc:
+    n = int(input())
+
+    a = list(map(int, input().split()))
+    b = list(map(int, input().split()))
+
+    for i in range(n):
+        if a[i] > b[i]:
+            a[i], b[i] = b[i], a[i]
+
+    print(max(a) * max(b))
+
+    tc -= 1
